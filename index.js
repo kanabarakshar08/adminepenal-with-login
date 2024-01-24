7b261e4879491a5f39d85d84a62008fbe6d725a1
kanabarakshar08 <136910034+kanabarakshar08@users.noreply.github.com>
2024-01-24 17:06:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const port = 8091;
 const app = express();
 const path = require('path');
-const db = require('./config/mongoose');
-
+// const db = require('./config/mongoose');
+const mongoose = require('mongoose');
+mongoose.connect(("mongodb+srv://kanabarakshar08:AKSHAR@akshar.7qjb0c5.mongodb.net/Login"), {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(() => console.log('Database Connected'))
+    .catch((err) => console.log(err));
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 const passport = require('passport');
